dfea21df9f7e5026cd617c0a1708e7c717474883
volkziem <90516537+volkziem@users.noreply.github.com>
2022-04-24 12:08:27 +0200

    sign of psi fixed

--- Jmrs_sigma.m
+++ Jmrs_sigma.m
@@ -6,7 +6,7 @@ denom=sqrt(det(dd));
 ddinv=inv(dd);
 psi=-1i*m*kappa*X'*X-2*m^2*kappa^2*X'*ddinv*sigma*X;
 Y=ddinv*X;
-factor=exp(-1i*m*mu-psi)/denom;
+factor=exp(-1i*m*mu+psi)/denom;        % sign of psi fixed, 220424
 out=factor*(ddinv*sigma+Y*Y') ;        % eq.34
 Xhat=factor*(Y(1)+1i*Y(2));    % eq.18, if m=n
 end
